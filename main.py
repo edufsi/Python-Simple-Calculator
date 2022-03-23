4e0fbeca1f1ca0be8c45f3459e08a62cf846fe32
Eduardo Fonseca da Silva <edufon1503@gmail.com>
2022-03-23 17:14:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def aplication():
     frame_2.place(relx=0.2, rely=0.3, relwidth=0.6, relheight=0.6)
     
     
-    def numero(x):
+    def apertar_botao(x):
         global text
         text = text + str(x)
         contas.set(text)
@@ -45,61 +45,63 @@ def aplication():
         
     def igual_a():
         global text
-        text = re.sub(r'x', '*')
-        total = eval(text)
-        text = str(total)
-        contas.set(total)
+        try:
+            text = re.sub(r'x', '*', text)
+            total = eval(text)
+            text = str(total)
+            contas.set(total)
+        except SyntaxError:
+            text = ''
+            contas.set('Erro Aritmético')
+        
         
 
-    botao_0 = Button(frame_2, text= '0', command=lambda: numero(0))
+    botao_0 = Button(frame_2, text= '0', command=lambda: apertar_botao(0))
     botao_0.place(relx=0.1, rely=0.1, relwidth=0.15,relheight=0.15)
 
-    botao_1 = Button(frame_2, text='1', command=lambda: numero(1))
+    botao_1 = Button(frame_2, text='1', command=lambda: apertar_botao(1))
     botao_1.place(relx=0.26, rely=0.1, relwidth=0.15, relheight=0.15)
 
-    botao_2 = Button(frame_2, text='2',  command=lambda: numero(2))
+    botao_2 = Button(frame_2, text='2',  command=lambda: apertar_botao(2))
     botao_2.place(relx=0.42, rely=0.1, relwidth=0.15, relheight=0.15)
 
-    botao_3 = Button(frame_2, text='3',  command=lambda: numero(3))
+    botao_3 = Button(frame_2, text='3',  command=lambda: apertar_botao(3))
     botao_3.place(relx=0.58, rely=0.1, relwidth=0.15, relheight=0.15)
 
-    mais = Button(frame_2, text='+', command=lambda: numero('+'))
+    mais = Button(frame_2, text='+', command=lambda: apertar_botao('+'))
     mais.place(relx=0.74, rely=0.1, relwidth=0.15, relheight=0.15)
 
 
-    botao_vezes = Button(frame_2, text='x', command=lambda: numero('*'))
+    botao_vezes = Button(frame_2, text='x', command=lambda: apertar_botao('x'))
     botao_vezes.place(relx=0.1, rely=0.27, relwidth=0.15, relheight=0.15)
 
-    botao_4 = Button(frame_2, text='4',  command=lambda: numero(4))
+    botao_4 = Button(frame_2, text='4',  command=lambda: apertar_botao(4))
     botao_4.place(relx=0.26, rely=0.27, relwidth=0.15, relheight=0.15)
 
-    botao_5 = Button(frame_2, text='5', command=lambda: numero(5))
+    botao_5 = Button(frame_2, text='5', command=lambda: apertar_botao(5))
     botao_5.place(relx=0.42, rely=0.27, relwidth=0.15, relheight=0.15)
 
-    botao_6 = Button(frame_2, text='6',  command=lambda: numero(6))
+    botao_6 = Button(frame_2, text='6',  command=lambda: apertar_botao(6))
     botao_6.place(relx=0.58, rely=0.27, relwidth=0.15, relheight=0.15)
 
-    menos = Button(frame_2, text='-', command=lambda: numero('-'))
+    menos = Button(frame_2, text='-', command=lambda: apertar_botao('-'))
     menos.place(relx=0.74, rely=0.27, relwidth=0.15, relheight=0.15)
 
+    divisão = Button(frame_2, text='/', command= lambda: apertar_botao('/'))
+    divisão.place(relx=0.1, rely=0.43, relwidth=0.15, relheight=0.15)
 
-    quadrado = Button(frame_2, text='a²')
-    quadrado.place(relx=0.1, rely=0.43, relwidth=0.15, relheight=0.15)
-
-    botao_7 = Button(frame_2, text='7',  command=lambda: numero(7))
+    botao_7 = Button(frame_2, text='7',  command=lambda: apertar_botao(7))
     botao_7.place(relx=0.26, rely=0.43, relwidth=0.15, relheight=0.15)
 
-    botao_8 = Button(frame_2, text='8',  command=lambda: numero(8))
+    botao_8 = Button(frame_2, text='8',  command=lambda: apertar_botao(8))
     botao_8.place(relx=0.42, rely=0.43, relwidth=0.15, relheight=0.15)
 
-    botao_9 = Button(frame_2, text='9',  command=lambda: numero(9))
+    botao_9 = Button(frame_2, text='9',  command=lambda: apertar_botao(9))
     botao_9.place(relx=0.58, rely=0.43, relwidth=0.15, relheight=0.15)
 
     igual = Button(frame_2, text='=', command=lambda: igual_a())
     igual.place(relx=0.74, rely=0.43, relwidth=0.15, relheight=0.15)
 
-    # label = Label(frame_2, text='Teste') LABEL É UM TEXTO SEM INTERAÇÃO
-    # label.place(relx=0.1, rely=0.59)
 
 
 
